b00e1e2679759373f66292e6f2b22cd9e0eacf37
aosmolowski <osm@ukr.net>
2017-02-25 16:34:48 +0200

    Правка коду.

--- app/src/main/java/osmolowski/aleksandr/activitystart/MainActivity.java
+++ app/src/main/java/osmolowski/aleksandr/activitystart/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //
+
 
         TextView view = (TextView) findViewById(R.id.first_tv);
         view.getText();
