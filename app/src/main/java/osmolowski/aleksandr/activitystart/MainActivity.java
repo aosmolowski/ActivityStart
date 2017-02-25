f3af618bbb33aac119eaa8057765564fae0e4948
aosmolowski <osm@ukr.net>
2017-02-25 16:21:12 +0200

    Довивантажуємо код.

---
+++ app/src/main/java/osmolowski/aleksandr/activitystart/MainActivity.java
@@ -0,0 +1,19 @@
+package osmolowski.aleksandr.activitystart;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //
+
+        TextView view = (TextView) findViewById(R.id.first_tv);
+        view.getText();
+        view.toString();
+    }
+}
